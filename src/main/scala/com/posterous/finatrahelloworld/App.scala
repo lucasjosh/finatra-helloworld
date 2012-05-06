b6f2b6ddabc93d09fe281a1a4bec7797e0121480
Julio Capote <julio@twitter.com>
2012-05-06 14:56:17 -0700

    fully working example

--- src/main/scala/com/posterous/finatrahelloworld/App.scala
+++ src/main/scala/com/posterous/finatrahelloworld/App.scala
@@ -1,12 +1,29 @@
 package com.posterous.finatrahelloworld
 
 import com.posterous.finatra.{FinatraApp, FinatraServer}
+import com.capotej.finatra_core.MultipartItem
 
 object App {
 
   class HelloWorld extends FinatraApp {
+
     get("/") { request =>
-      response("hey")
+      response(body="hey")
+    }
+
+    post("/form") { request =>
+      request.multiParams.get("foo") match {
+        case Some(item) => response(body=item.data.toString)
+        case None => response(status=500, body="incorrect params sent")
+      }
+    }
+
+    get("/templatest") { request =>
+      render(path="lol.mustache", exports=Map("foo" -> "bar"))
+    }
+
+    get("/a/b/:c") { request =>
+      response(body=request.params.get("c").getOrElse("none"))
     }
 
   }
